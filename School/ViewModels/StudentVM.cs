faf728c1861384d4cee8952070957f3dcee87252
adriancalavie <adrian.calavie06@gmail.com>
2021-05-24 02:56:18 +0300

    Added BaseVM and StudentVM

--- School/ViewModels/StudentVM.cs
+++ School/ViewModels/StudentVM.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace School.ViewModels
 {
-    class StudentVM
+    class StudentVM: BaseVM
     {
+
+        private int _Id;
+        public int Id
+        {
+            get => _Id;
+            set => SetProperty(ref _Id, value);
+        }
+
+
+        private bool _IsDebtor;
+        public bool IsDebtor
+        {
+            get => _IsDebtor;
+            set => SetProperty(ref _IsDebtor, value);
+        }
+
+
+        private double _AnnualGrade;
+        public double AnnualGrade
+        {
+            get => _AnnualGrade;
+            set => SetProperty(ref _AnnualGrade, value);
+        }
+
+
+        //private int _FkClass;
+        //public int FkClass
+        //{
+        //    get => _FkClass;
+        //    set => SetProperty(ref _FkClass, value);
+        //}
+
     }
 }
